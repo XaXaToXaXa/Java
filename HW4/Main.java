3cdb873c917fc1781105dfc61caedaa16a400552
xaxatoxaxa <xaxatoxaxa@gmail.com>
2023-04-05 16:09:12 +0300

    Add HW4 Task2

--- HW4/Main.java
+++ HW4/Main.java
@@ -5,13 +5,25 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
         //Task 1
-        LinkedList<Integer> taskList = Task1.fillList(11);
-        System.out.println("Start list: ");
-        Task1.printList(taskList);
-        System.out.println("-------");
-        System.out.println("Reversed list: ");
-        Task1.printList(Task1.reversList(taskList));
-        System.out.println("-------");
-
+//        LinkedList<Integer> taskList = Task1.fillList(11);
+//        System.out.println("Start list: ");
+//        Task1.printList(taskList);
+//        System.out.println("-------");
+//        System.out.println("Reversed list: ");
+//        Task1.printList(Task1.reversList(taskList));
+//        System.out.println("-------");
+        //Task 2
+        LinkedList<Integer> task2List = Task1.fillList(11);
+        System.out.println("First list: ");
+        Task1.printList(task2List);
+        Task2.enqueue(task2List);
+        System.out.println("List with added number: ");
+        Task1.printList(task2List);
+        Task2.dequeue(task2List);
+        System.out.println("List after removing first number: ");
+        Task1.printList(task2List);
+        Task2.first(task2List);
+        System.out.println("Final list: ");
+        Task1.printList(task2List);
     }
 }
---
+++ HW4/Task2.java
@@ -0,0 +1,25 @@
+package HW4;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+//Реализуйте очередь с помощью LinkedList со следующими методами:
+//        • enqueue() — помещает элемент в конец очереди,
+//        • dequeue() — возвращает первый элемент из очереди и удаляет его,
+//        • first() — возвращает первый элемент из очереди, не удаляя.
+public class Task2 {
+    public static void enqueue(LinkedList<Integer> list) {
+        System.out.println("Введите элемент, который надо добавить: ");
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        list.addLast(i);
+        scan.close();
+    }
+    public static void dequeue(LinkedList<Integer> list){
+        int i = list.pollFirst();
+        System.out.println("Number to remove from list: " + i);
+    }
+    public static void first(LinkedList<Integer> list){
+        System.out.println("First element in list: " + list.peekFirst());
+    }
+}
