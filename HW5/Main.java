d50478df822d00cbe9eedf544851988ffe45f480
xaxatoxaxa <xaxatoxaxa@gmail.com>
2023-04-10 18:09:36 +0300

    Add HW5 Task4

--- HW5/Main.java
+++ HW5/Main.java
@@ -10,12 +10,15 @@ public class Main {
         //Task2
 //        Task2.nameList();
         //Task3
-        int[] myArray = Task3.fillArray(20);
-        System.out.println("Исходный массив: ");
-        Task3.printArray(myArray);
-        Task3.sortArray(myArray);
-        System.out.println("Отсортированный массив: ");
-        Task3.printArray(myArray);
-
+//        int[] myArray = Task3.fillArray(20);
+//        System.out.println("Исходный массив: ");
+//        Task3.printArray(myArray);
+//        Task3.sortArray(myArray);
+//        System.out.println("Отсортированный массив: ");
+//        Task3.printArray(myArray);
+        //Task4
+        Task4.Board board = new Task4.Board (8);
+        System.out.println("\nВариант расположения ферзей: \n ");
+        Task4.Board.nextTurn(board);
     }
 }
