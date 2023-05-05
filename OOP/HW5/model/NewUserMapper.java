03bcb91031329f072acf8ce9ddaf0796451ca2cf
xaxatoxaxa <xaxatoxaxa@gmail.com>
2023-05-05 16:54:48 +0300

    OOP HW5

--- OOP/HW5/model/NewUserMapper.java
+++ OOP/HW5/model/NewUserMapper.java
@@ -1,14 +1,14 @@
 package OOP.HW5.model;
 
 public class NewUserMapper extends UserMapper{
-    public String map(User user, String delimiter) {
-        String text = String.join(delimiter, user.getId(), user.getFirstName(), user.getLastName(), user.getPhone());
+    public String map(User user, String splitter) {
+        String text = String.join(splitter, user.getId(), user.getFirstName(), user.getLastName(), user.getPhone());
         return text;
     }
 
 
-    public User map(String line, String delimiter) {
-        String[] lines = line.split(delimiter);
+    public User map(String line, String splitter) {
+        String[] lines = line.split(splitter);
         return new User(lines[0], lines[1], lines[2], lines[3]);
     }
 }
